52c5080992df3273204c2f1fd88340ea66047fcc
TimothyFell <timothy.fell846@gmail.com>
2019-01-30 17:17:54 -0700

    Write happy path test for get food index endpoint

--- test/post_request.spec.js
+++ test/post_request.spec.js
@@ -47,5 +47,23 @@ describe('API Routes', () => {
       })
     });
 
-  })
+  });
+
+  describe('get api/v1/foods', () => {
+    it("should return all food entries in database", done =>  {
+      chai.request(server)
+      .get("/api/v1/foods")
+      .end((err, response) => {
+        response.should.have.status(200);
+        var foods = JSON.parse(response.body)
+        console.log(foods)
+        foods[0].should.have.property('id')
+        foods[0].should.have.property('name')
+        foods[0].should.have.property('calories')
+        foods[0].name.should.equal('goodburger')
+        foods[0].calories.should.equal(42)
+        done();
+      })
+    });
+  });
 });
