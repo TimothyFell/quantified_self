14ff8e5a5eff004c31eb7f48256c33be3c7e9b38
TimothyFell <timothy.fell846@gmail.com>
2019-01-30 17:18:32 -0700

    Add handler for get food endpoint

--- server.js
+++ server.js
@@ -47,5 +47,15 @@ app.post('/api/v1/foods', (request, response) => {
     });
 });
 
+app.get('/api/v1/foods', (request, response) => {
+  database('foods').select()
+    .then((foods) => {
+      response.status(200).json(JSON.stringify(foods));
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 
 module.exports = app;
