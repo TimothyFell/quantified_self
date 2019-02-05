f85b69d5589a1ffd40cd34a78f5ec9f3efb81489
TimothyFell <timothy.fell846@gmail.com>
2019-02-05 07:31:15 -0700

    Add correct database information from heroku to knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,15 +16,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: "postgres://localhost/quantified_self_be",
+    connection: "postgres://shyfiiginameca:1ea250574fa2ae444a6b3966f9b3c51e4cf7e820ba85e0c387e0241611e255b6@ec2-54-235-67-106.compute-1.amazonaws.com:5432/dc4i5c4cdvmlt6",
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/dev'
     },
-    ssl: true,
-    useNullAsDefault: true
+    ssl: true
   }
 
 };
