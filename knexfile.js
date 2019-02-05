7bc7d887031f01e0718b11ac4c64e6d87022a0d0
TimothyFell <timothy.fell846@gmail.com>
2019-02-04 17:14:47 -0700

    Update production section of knexfile to connect to correct database

--- knexfile.js
+++ knexfile.js
@@ -15,19 +15,16 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: "postgres://localhost/quantified_self_be",
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    ssl: true,
+    useNullAsDefault: true
   }
 
 };
